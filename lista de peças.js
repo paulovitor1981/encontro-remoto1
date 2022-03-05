b56f2a462b0b93e8acbc4a9df08e989f11d1d7c3
paulovitor1981 <paulo.cdvitor@hotmail.com>
2022-03-05 18:03:30 -0300

    encontro remoto 1

---
+++ lista de peças.js
@@ -0,0 +1,30 @@
+var listadepecas = ["Amortecedor", "Motor", "Filtro de ar"]
+
+
+if(listadepecas.length < 10){
+    console.log("Ainda tem espaço")
+} 
+    else{
+        console.log("não tem mais espaço")
+    }    
+
+
+let peso = 99;
+if(peso < 100){
+    console.log("A peça deve pesar no minimo de 100g")
+} 
+    else{
+        console.log("A peça esta no peso adequado")
+    }    
+
+    let nomePeca = "";
+if(nomePeca.length > 3){
+    console.log("Nome da peça esta adequado")
+} 
+    else if (nomePeca.length== 0){
+        console.log("O nome da peça não pode ser vazio")
+    }    
+    else{
+        console.log("Nome da peça precisa ter no minimo 3 caracteres ")
+    }
+
